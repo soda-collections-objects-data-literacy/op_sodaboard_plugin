75216ff350223fe941249070d0573507c462b3af
Johannes Schäffer <schaeffer.johannes@gmail.com>
2025-03-16 02:59:58 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   scope '', as: 'kitten_plugin' do
     scope 'projects/:project_id', as: 'project' do
-      resources :kittens
+      #resources :kittens
+      get 'roadmap', to: "kittens#index"
     end
   end
 
