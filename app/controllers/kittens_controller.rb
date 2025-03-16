5a8b4d62f3a46556cc5c866380a1dec7a5a79a79
Johannes Schäffer <schaeffer.johannes@gmail.com>
2025-03-16 02:37:07 +0100

    basic functionality

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -8,7 +8,8 @@ class KittensController < ApplicationController
     @kittens = []#Kitten.all
     cat_event = @project.categories.find_by name: "Event"
     cat_contrib = @project.categories.find_by name: "Beitrag zu Veranstaltung (extern)"
-    @wps = @project.work_packages.where(category_id: [cat_event.id,cat_contrib.id]).order(:subject)#.where(is_closed: false,)
+    event_wps = @project.work_packages.where(category_id: [cat_event.id,cat_contrib.id])#.where(is_closed: false,)
+    contrib_wps = @project.work_packages.where(category_id: [cat_contrib.id])#.where(is_closed: false,)
     @roadmap_hashes = []
     
     start_date = Date.new(2025, 1, 1)
@@ -18,14 +19,22 @@ class KittensController < ApplicationController
     
     (0..number_of_months).each do |month|
       m_date = start_date + month.months
+      wp_month_datestr = m_date.strftime("%Y-%m")
+      month_events = event_wps.select{|wp| wp.subject.starts_with?(wp_month_datestr)}.sort_by {|obj| obj.subject}
+      month_workshops = month_events.select{|wp| wp.subject.include?("Workshop")}
+      month_forum = month_events.select{|wp| wp.subject.include?("Forum")}
+      month_sonst = month_events
+      
+      month_contrib = contrib_wps.select{|wp| wp.subject.starts_with?(wp_month_datestr)}.sort_by {|obj| obj.subject}
+      
       m_hash = {
         label: m_date.strftime("%b %Y"),
-        workshops: [],
-        forum: [],
-        sonst_event: [],
-        contrib_extern: [],
-        
+        workshops: month_workshops,
+        forum: month_forum,
+        sonst_event: month_sonst,
+        contrib_extern: month_contrib
       }
+      
       @roadmap_hashes << m_hash
     end
     @table_rows = [:workshops,:forum,:sonst_event,:contrib_extern]
